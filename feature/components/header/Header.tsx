e870c6a6068fc8c35f4bdfe048499726bb890217
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-07-02 02:10:00 +0400

    update

--- feature/components/header/Header.tsx
+++ feature/components/header/Header.tsx
@@ -1,14 +1,55 @@
+"use client";
 import {HeaderList} from "@/feature/data/data";
 import Image from "next/image";
 import Link from "next/link";
-
+import {motion} from "motion/react";
+import {useEffect, useState} from "react";
 const Header = () => {
+  const [isUp, setIsUp] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      if (window.innerWidth > 1000) {
+        setIsUp(false);
+      }
+    };
+    checkScreenSize();
+
+    window.addEventListener("resize", checkScreenSize);
+
+    return;
+  }, []);
+
   return (
     <div>
-      <header className="lg:py-[24px] py-[13px] w-full lg:flex lg:justify-center">
+      <header className="lg:py-[24px] py-[13px] relative bg-[#f6f5f4] z-20 overflow-hidden w-full lg:flex lg:justify-center">
         <div className="max-w-[1180px] justify-between w-full flex min-h-[50px] px-[20px] items-center">
-          <div className="cursor-pointer flex lg:hidden">
-            <Image src="/Burger.svg" alt="burgerMenu" width={24} height={24} />
+          <div>
+            {isUp ? (
+              <button
+                className="cursor-pointer flex lg:hidden"
+                onClick={() => setIsUp(!isUp)}
+              >
+                <Image
+                  src="/close.svg"
+                  alt="burgerMenu"
+                  width={24}
+                  height={24}
+                />
+              </button>
+            ) : (
+              <button
+                className="cursor-pointer flex lg:hidden"
+                onClick={() => setIsUp(!isUp)}
+              >
+                <Image
+                  src="/Burger.svg"
+                  alt="burgerMenu"
+                  width={24}
+                  height={24}
+                />
+              </button>
+            )}
           </div>
           <div className="flex gap-[30px] items-center">
             <Link href="/">
@@ -58,6 +99,23 @@ const Header = () => {
         </div>
       </header>
       <div className="w-full h-[1px] bg-[#E0E0E0]"></div>
+      <motion.div
+        className="h-dvh w-full bg-[#f6f5f4] z-1 absolut flex items-center justify-center"
+        initial={{y: "-150%"}}
+        animate={{y: isUp ? "0" : "-150%"}}
+        transition={{duration: 1}}
+      >
+        <ul className="flex flex-col gap-[32px] text-[#757575]">
+          {HeaderList.map((eacheElement) => (
+            <li
+              key={eacheElement.id}
+              className="text-center hover:text-[#2B788B] font-semibold transition-all duration-300 ease-in-out"
+            >
+              <a href={eacheElement.link}>{eacheElement.page}</a>
+            </li>
+          ))}
+        </ul>
+      </motion.div>
     </div>
   );
 };
