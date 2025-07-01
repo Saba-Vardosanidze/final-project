21e2a922e3721dc213d7b84fe966e99c73b06a64
Saba-Vardosanidze <sabavardo674@gmail.com>
2025-07-01 14:22:26 +0400

    update

---
+++ feature/data/data.ts
@@ -0,0 +1,22 @@
+export const HeaderList = [
+  {
+    id: 1,
+    page: "Home",
+    link: "#",
+  },
+  {
+    id: 2,
+    page: "Textbook",
+    link: "#",
+  },
+  {
+    id: 3,
+    page: "Statistics",
+    link: "#",
+  },
+  {
+    id: 4,
+    page: "Games",
+    link: "#",
+  },
+];
